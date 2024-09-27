7c54e40a887290a5a50f860a1e1f84674c0d879a
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-09-27 17:44:16 +0530

    Add basic views & templates

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+]
 
 # Serve media files during development
 if settings.DEBUG:
