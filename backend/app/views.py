0b78b23e04fae97edb7343b701ff55c8b4fc8b61
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-09-29 19:10:23 +0530

    Fix logout api

--- backend/app/views.py
+++ backend/app/views.py
@@ -13,15 +13,20 @@ from rest_framework import viewsets, generics
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 
 from django.contrib.auth.models import User
 
 
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def fish_list(request):
     fishes = Fish.objects.all()  # Retrieve all fish entries
     return render(request, "fish/index.html", {"fishes": fishes})
 
 
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def fish_detail(request, fish_id):
     fish = get_object_or_404(Fish, id=fish_id)
     return render(request, "fish/fish_detail.html", {"fish": fish})
@@ -47,15 +52,26 @@ class RegisterView(APIView):
             status=status.HTTP_201_CREATED,
         )
 
+
 class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data.get("refresh_token")
+            if refresh_token is None:
+                return Response(
+                    {"error": "Refresh token is required"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             token = RefreshToken(refresh_token)
             token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
+            return Response(
+                {"success": "User logged out successfully"}, status=status.HTTP_200_OK
+            )
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "app",
     "rest_framework",
-    'rest_framework_simplejwt',
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -145,4 +147,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-DJOSER = {"USER_ID_FIELD": "username"}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
