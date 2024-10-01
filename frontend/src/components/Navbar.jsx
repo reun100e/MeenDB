20650616291cd9390be36d02b46ad87000c70bd6
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-01 07:39:33 +0530

    Add Navbar.jsx and some assets

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link to="/" className="navbar-logo-link">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </Link>
+      <ul className="navbar-menu-left">
+        <li>
+          <Link to="/why">Why MeenDB?</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <ul className="navbar-menu-right">
+        {isAuthorized ? (
+          <li>
+            <Link onClick={handleLogout} to="/logout" className="button-link">
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login" className="button-link-login">
+                Log In
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="button-link">
+                Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
