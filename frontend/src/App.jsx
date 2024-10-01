a579e23b9e7be14c39cbd9cac76b251380eb6463
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-01 10:24:49 +0530

    Add NotFound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
