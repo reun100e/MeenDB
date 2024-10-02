5fdc7805b0a3df8fdbdf7b4f59707ca8be1a2d91
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-02 18:57:43 +0530

    update css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,10 +6,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <img src={img} alt="Background woman" className="header-bg" />
+      <img src={img} alt="Image of fish" className="header-bg" />
       <div className="header-content">
-        <h1>ചാള or മത്തി?</h1>
-        <h3>MeenDB has the answer</h3>
+        <h1>ചാള or മത്തി ?</h1>
+        <h2>MeenDB has the answer</h2>
         <Link to="/fish">
           {" "}
           <button>Explore MeenDB</button>
