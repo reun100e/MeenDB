a25887565c0c36c426dd22a3b3d7a93b8023fe30
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-11 17:40:50 +0530

    update admin.py to new schema

--- backend/fish/admin.py
+++ backend/fish/admin.py
@@ -1,8 +1,75 @@
 from django.contrib import admin
-from .models import Fish, LocalName, FishPhoto
+from .models import (
+    Fish,
+    CommonNames,
+    FishPhoto,
+    Family,
+    ExternalLinks,
+    PhotoDetails,
+    CustomUser,
+)
 
-# Register your models here.
 
-admin.site.register(Fish)
-admin.site.register(LocalName)
-admin.site.register(FishPhoto)
+class CommonNamesInline(admin.TabularInline):
+    model = CommonNames
+    extra = 1
+
+
+class ExternalLinksInline(admin.TabularInline):
+    model = ExternalLinks
+    extra = 1
+
+
+class FishPhotoInline(admin.TabularInline):
+    model = FishPhoto
+    extra = 1
+
+
+@admin.register(Fish)
+class FishAdmin(admin.ModelAdmin):
+    list_display = ("scientific_name", "family", "added_by")
+    list_filter = ("family", "added_by")
+    search_fields = ("scientific_name", "common_names__common_name")
+    inlines = [CommonNamesInline, ExternalLinksInline, FishPhotoInline]
+
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = ("family_name",)
+    search_fields = ("family_name",)
+
+
+@admin.register(FishPhoto)
+class FishPhotoAdmin(admin.ModelAdmin):
+    list_display = ("fish", "added_by", "uploaded_at")
+    list_filter = ("fish", "added_by")
+    search_fields = ("fish__scientific_name", "description")
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "user_type", "is_staff", "is_active")
+    list_filter = ("user_type", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "user_type",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(CommonNames)
+admin.site.register(ExternalLinks)
+admin.site.register(PhotoDetails)
