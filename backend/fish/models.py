e1acd50a652923d3fe9bc0c19f71af20f8348da4
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-11 17:45:17 +0530

    Add CustomUser to models.py

--- backend/fish/models.py
+++ backend/fish/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .users import CustomUser
+
+User = get_user_model()
 
 
 class Family(models.Model):
