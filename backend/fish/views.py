31afd519b6c228075e327332e7652950ad5620e1
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-11 17:36:24 +0530

    update views to new schema

--- backend/fish/views.py
+++ backend/fish/views.py
@@ -1,25 +1,80 @@
-from rest_framework import viewsets
-from .models import Fish, LocalName, FishPhoto
-from .serializers import FishSerializer, LocalNameSerializer, FishPhotoSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, permissions
+from .models import (
+    Fish,
+    CommonNames,
+    FishPhoto,
+    Family,
+    ExternalLinks,
+    PhotoDetails,
+    CustomUser,
+)
+from .serializers import (
+    FishSerializer,
+    CommonNamesSerializer,
+    FishPhotoSerializer,
+    FamilySerializer,
+    ExternalLinksSerializer,
+    PhotoDetailsSerializer,
+    UserSerializer,
+)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.added_by == request.user
 
 
 class FishViewSet(viewsets.ModelViewSet):
     queryset = Fish.objects.all()
     serializer_class = FishSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(added_by=self.request.user)
 
 
-class LocalNameViewSet(viewsets.ModelViewSet):
-    queryset = LocalName.objects.all()
-    serializer_class = LocalNameSerializer
-    permission_classes = [IsAuthenticated]
+class CommonNamesViewSet(viewsets.ModelViewSet):
+    queryset = CommonNames.objects.all()
+    serializer_class = CommonNamesSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
 
 
 class FishPhotoViewSet(viewsets.ModelViewSet):
     queryset = FishPhoto.objects.all()
     serializer_class = FishPhotoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+class FamilyViewSet(viewsets.ModelViewSet):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ExternalLinksViewSet(viewsets.ModelViewSet):
+    queryset = ExternalLinks.objects.all()
+    serializer_class = ExternalLinksSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+class PhotoDetailsViewSet(viewsets.ModelViewSet):
+    queryset = PhotoDetails.objects.all()
+    serializer_class = PhotoDetailsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
