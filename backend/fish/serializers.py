19408db146b32a1cc0383addf79cf9f530fedecf
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-13 13:46:17 +0530

    Revert "Refactored and Reorgnaized backend apps"
    This reverts commit 9d57c09db0cb0f002a8a96cbb4bc5c96c6b4115b.

--- backend/fish/serializers.py
+++ backend/fish/serializers.py
@@ -1,20 +1,61 @@
 from rest_framework import serializers
-from .models import Fish, LocalName, FishPhoto
+from .models import (
+    Fish,
+    CommonNames,
+    FishPhoto,
+    Family,
+    ExternalLinks,
+    PhotoDetails,
+    CustomUser,
+)
+
+
+class FamilySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Family
+        fields = "__all__"
+
+
+class CommonNamesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommonNames
+        fields = "__all__"
+
+
+class ExternalLinksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExternalLinks
+        fields = "__all__"
+
+
+class PhotoDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoDetails
+        fields = "__all__"
+
+
+class FishPhotoSerializer(serializers.ModelSerializer):
+    details = PhotoDetailsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = FishPhoto
+        fields = "__all__"
 
 
 class FishSerializer(serializers.ModelSerializer):
+    family = FamilySerializer(read_only=True)
+    common_names = CommonNamesSerializer(many=True, read_only=True)
+    external_links = ExternalLinksSerializer(many=True, read_only=True)
+    photos = FishPhotoSerializer(many=True, read_only=True)
+    added_by = serializers.ReadOnlyField(source="added_by.username")
+
     class Meta:
         model = Fish
         fields = "__all__"
 
 
-class LocalNameSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = LocalName
-        fields = "__all__"
-
-
-class FishPhotoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FishPhoto
-        fields = "__all__"
+        model = CustomUser
+        fields = ["id", "username", "email", "user_type"]
+        read_only_fields = ["user_type"]
